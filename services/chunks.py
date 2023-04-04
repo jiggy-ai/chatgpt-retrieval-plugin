edc545054957f33ef6cf5ed5a09f3ba0c0652a0e
Bill Kish <bill@jiggy.ai>
2023-04-03 20:45:49 -0700

    initial config for chunking

--- services/chunks.py
+++ services/chunks.py
@@ -2,6 +2,7 @@ from loguru import logger
 from typing import Dict, List, Optional, Tuple
 import uuid
 from models.models import Document, DocumentChunk, DocumentChunkMetadata
+from server.config import chunk_config
 
 import tiktoken
 
@@ -12,12 +13,14 @@ tokenizer = tiktoken.get_encoding(
     "cl100k_base"
 )  # The encoding scheme to use for tokenization
 
-# Constants
-CHUNK_SIZE = 200  # The target size of each text chunk in tokens
-MIN_CHUNK_SIZE_CHARS = 350  # The minimum size of each text chunk in characters
-MIN_CHUNK_LENGTH_TO_EMBED = 5  # Discard chunks shorter than this
-EMBEDDINGS_BATCH_SIZE = 128  # The number of embeddings to request at a time
-MAX_NUM_CHUNKS = 10000  # The maximum number of chunks to generate from a text
+
+# Set constants from chunk_config
+logger.info(chunk_config)
+CHUNK_SIZE                = chunk_config.chunk_size                 # The target size of each text chunk in tokens
+MIN_CHUNK_SIZE_CHARS      = chunk_config.min_chunk_size_chars       # The minimum size of each text chunk in characters
+MIN_CHUNK_LENGTH_TO_EMBED = chunk_config.min_chunk_length_to_embed  # Discard chunks shorter than this
+EMBEDDINGS_BATCH_SIZE     = chunk_config.embeddings_batch_size      # The number of embeddings to request at a time
+MAX_NUM_CHUNKS            = chunk_config.max_num_chunks             # The maximum number of chunks to generate from a text
 
 
 def get_text_chunks(text: str, chunk_token_size: Optional[int]) -> List[str]:
